9b761944bf7fb00d1e77b8cda75bb91b84849ce3
Ben Hensor <benhensor@gmail.com>
2023-03-15 13:28:41 +0000

    added html and js files to repo

---
+++ newFolder/main.js
@@ -0,0 +1,15 @@
+console.log("Hello World!")
+console.log("This is working!")
+
+if (1 > 3) {
+  console.log("Well, duh! Of course 1 isn't greater than three...");
+}
+
+if (3 > 1) {
+  console.log("Of Course!");
+}
+
+let numberOfPies = getPieCount();
+if (numberOfPies < 1) {
+  console.log("Who ate all the pies???");
+}
